67726987138311ffad001b99e0f144da89c8186c
Joel James <129211529+joeljamesv@users.noreply.github.com>
2026-05-13 07:52:59 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+   public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int maxFrequency = nums.size();
+        std::unordered_map<int, int> hash;
+        std::vector<int> retList(k,0);
+        for (int i = 0; i < maxFrequency; i++) {
+            if (hash.find(nums[i]) != hash.end()) {
+                hash[nums[i]]++;
+            } else
+                hash.insert({nums[i], 1});
+        }
+        
+        int maxValue;
+        int maxKey;
+        for(int i=0;i<k;i++)
+        {
+            maxValue = 0;
+            for(auto &[key,value] : hash)
+            {
+                if(value > maxValue)
+                {
+                    maxValue = value;
+                    maxKey = key;
+                }
+            }
+            std::cout << "maxKey/Val "<<maxKey<<" "<<maxValue<<"\n";
+            retList[i] = maxKey;
+            hash[maxKey]=0;
+        }
+        
+        for(auto &i : retList)
+            std::cout << i << std::endl;
+
+        return retList;
+    }
+};
