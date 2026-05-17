8704db2ac5b352692be3a7907b0c0341a7ed7bb9
Joel James <129211529+joeljamesv@users.noreply.github.com>
2026-05-17 13:48:18 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if len(piles) == h:
+            return max(piles)
+
+        minSpeed = 1
+        maxSpeed = max(piles)
+        retSpeed = maxSpeed
+
+        while minSpeed <= maxSpeed:
+            avgSpeed = (minSpeed + maxSpeed) // 2
+            print(avgSpeed)
+            hoursTaken = self.eatable(piles, avgSpeed)
+            print("totalHours ",hoursTaken)
+            if hoursTaken <= h:
+                maxSpeed = avgSpeed - 1
+                if avgSpeed < retSpeed:
+                    retSpeed = avgSpeed
+            else:
+                minSpeed = avgSpeed + 1
+        return retSpeed
+
+    def eatable(self, piles, avgSpeed) -> long:
+        totalHours = 0
+        for i in piles:
+            if i % avgSpeed == 0:
+                totalHours = totalHours + (i // avgSpeed)
+                print(i//avgSpeed)
+                print(totalHours)
+            else:
+                totalHours = totalHours + (i // avgSpeed) + 1
+                print(i//avgSpeed)
+                print(totalHours)
+
+        print(totalHours)
+        return int(totalHours)
